65f1af52dbb711b96c6920cea33ed2d3c074cf8f
SaraiMV <sarizvargas@gmail.com>
2022-04-21 21:33:44 -0500

    Adding endpoint GET /v1/explorers

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 // Usando objeto express
 const express = require('express')
+const { get } = require('express/lib/response')
 // App de Express
 const app = express()
 app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
+
+// Routes
+// Methods HTTP (GET, POST, PUT, DELETE)
+app.get('/v1/explorers', (req, res)=>{
+    console.log(`GET explorers V1 API ${new Date()}`)
+    const explorer1 = {id: 1, name: 'Sara'}
+    const explorer2 = {id: 2, name: 'Sara'}
+    const explorers =[explorer1, explorer2]
+    // HTTP CODE STATUS: 200
+    res.status(200).json(explorers)
+})
+
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
